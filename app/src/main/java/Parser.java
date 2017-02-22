03616e0a4a21c68761033cad73daecfbb19d4a15
Lukas f. Paluch <lukaspaluch@outlook.com>
2017-02-22 20:43:36 +0100

    initial dev commit

---
+++ app/src/main/java/Parser.java
@@ -0,0 +1,14 @@
+/**
+ * Created by lfp on 22.02.2017.
+ */
+
+public class Parser {
+    private String link;
+    public Parser(String link) {
+        this.link = link;
+    }
+
+    public void parse() {
+
+    }
+}
